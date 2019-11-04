7ea9eaad5fb6b7b5dead550f4a918cb956038b53
imogencs <40676815+imogencs@users.noreply.github.com>
2019-11-04 12:46:46 -0500

    Update and rename 1.0 to hw2.cpp

---
+++ hw2.cpp
@@ -0,0 +1,44 @@
+//============================================================================
+// Name        : hw2.cpp
+// Author      : Imogen&Jyalu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Lucas Number Calculator C++
+//============================================================================
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+void LucasPrint (int n, int val) {
+	cout << "L(" << n << ") = " << val << endl;
+}
+
+
+void LucasNumbers(int targetN, int currentN, int nMinusOne, int nMinusTwo) {
+	int thisNum = nMinusOne + nMinusTwo;
+	LucasPrint(currentN, thisNum);
+	if (targetN-1 >= currentN) {
+		LucasNumbers(targetN, currentN+1, thisNum, nMinusOne);
+	}
+}
+
+
+int main(int argc, char** argv) {
+	int n = 0;
+	if (argc > 1) {
+		n = atoi(argv[1]);
+	}
+
+	if (n >= 0) {
+		LucasPrint(0, 2);
+	}
+	if (n >= 1) {
+		LucasPrint(1, 1);
+	}
+	if (n > 1) {
+		LucasNumbers(n, 2, 1, 2);
+	}
+	return 0;
+}
