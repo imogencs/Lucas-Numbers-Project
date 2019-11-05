812329c7d560996581d90638ca34588f28848b26
jwu2018 <36602115+jwu2018@users.noreply.github.com>
2019-11-05 12:39:26 -0500

    Add files via upload
    previous time works!!!

--- LucasNumbers.cpp
+++ LucasNumbers.cpp
@@ -1,6 +1,6 @@
 //============================================================================
-// Name        : hw2.cpp
-// Author      : Imogen&Jyalu
+// Name        : LucasNumbers.cpp
+// Author      : Imogen Cleaver-Stigum & Jyalu Wu
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Lucas Number Calculator C++
@@ -8,52 +8,8 @@
 
 #include <iostream>
 #include <time.h>
+#include "LucasNumbers.h"
 using namespace std;
-
-
-void LucasPrint (int n, long long val, clock_t time, float pTime) {
-	float tTime = ((float) time) / CLOCKS_PER_SEC;
-
-	cout << "L(" << n << ") = " << val << "\t\t in " << tTime;
-	cout << "\tseconds.";
-	cout << "\tprevious time: " << pTime;
-	cout << "\t\tRatio: " << (tTime / pTime) << endl;
-}
-
-
-clock_t nextLucasNumber(int targetN, int currentN, long long nMinusOne, long long nMinusTwo,
-		clock_t currentTime, clock_t totalTime) {
-
-	long long thisNum = nMinusOne + nMinusTwo;
-	clock_t previousTime = totalTime;
-
-	currentTime = clock() - currentTime;
-	totalTime += currentTime;
-	LucasPrint(currentN, thisNum, totalTime, ((float) previousTime) / CLOCKS_PER_SEC);
-	if (targetN > currentN) {
-		totalTime = nextLucasNumber(targetN, currentN+1, thisNum, nMinusOne,
-				currentTime, totalTime);
-	}
-	return totalTime;
-}
-
-clock_t LucasNumbers(int n){
-	clock_t t = clock();
-	clock_t totalTime = t;
-	if (n >= 0) {
-		totalTime = clock() - t;
-		LucasPrint(0, 2, totalTime, t);
-	}
-	if (n >= 1) {
-		totalTime = clock() - t;
-		LucasPrint(1, 1, totalTime, t);
-	}
-	if (n > 1) {
-		totalTime = clock() - t;
-		totalTime = nextLucasNumber(n, 2, 1, 2, t, totalTime);
-	}
-	return totalTime;
-}
 
 
 int main(int argc, char** argv) {
@@ -66,18 +22,62 @@ int main(int argc, char** argv) {
 	// part 2 - Lucas nums up to n
 	clock_t totalTime = LucasNumbers(n);
 	t0 = clock() - t0;
-	cout << "It took " << ((float)totalTime)/CLOCKS_PER_SEC << " seconds to compute the first ";
-			cout << n << " Lucas Numbers. " << endl;
-//
-//
-//	// part 3 - up to n=30
-//	for (int i = 0; i < 30; i++) {
-//		clock_t ti = clock();
-//		LucasNumbers(i);
-//		ti = clock() - ti;
-//		cout << "It took " << ((float)ti)/CLOCKS_PER_SEC << " seconds to compute the first " << n << " Lucas Numbers. " << endl;
-//
-//	}
+//	cout << "It took " << ((float)totalTime)/CLOCKS_PER_SEC << " seconds to compute the first ";
+//			cout << n << " Lucas Numbers. " << endl;
 
 	return 0;
+}
+
+
+clock_t LucasNumbers(int n){
+	clock_t startTime = clock();
+	clock_t thisTime = startTime;
+	float prevTime = ((float) startTime)/CLOCKS_PER_SEC;
+
+	if (n >= 0) {
+		thisTime = clock() - startTime;
+		LucasPrint(0, 2, thisTime, prevTime);
+		prevTime = ((float) thisTime)/CLOCKS_PER_SEC;
+	}
+	if (n >= 1) {
+		thisTime = clock() - startTime;
+		LucasPrint(1, 1, thisTime, prevTime);
+	}
+	if (n > 1) {
+		thisTime = nextLucasNumber(n, 2, 1, 2, startTime, thisTime, prevTime);
+	}
+	return thisTime;
+}
+
+
+clock_t nextLucasNumber(int targetN, int currentN, long long nMinusOne, long long nMinusTwo,
+		clock_t startTime, clock_t thisTime, float prevTime) {
+
+	long long thisNum = nMinusOne + nMinusTwo;
+	prevTime = ((float) thisTime)/CLOCKS_PER_SEC;
+	thisTime = clock() - startTime;
+	LucasPrint(currentN, thisNum, thisTime, prevTime);
+
+	if (targetN > currentN) {
+		thisTime = nextLucasNumber(targetN, currentN+1, thisNum, nMinusOne,
+				startTime, thisTime, prevTime);
+	}
+	return thisTime;
+}
+
+
+void LucasPrint (int n, long long val, clock_t time, float prevTime) {
+	float tTime = ((float) time) / CLOCKS_PER_SEC;
+
+	cout << "L(" << n << ") = " << val << "\t\t in " << tTime;
+	cout << "\tseconds.";
+
+	if (n >= 1) {
+		cout << "\tprevious time: " << prevTime;
+		cout << "\t\tRatio: " << (tTime / prevTime) << endl;
+	}
+	else {
+		cout << endl;
+	}
+
 }
