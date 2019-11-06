58741d12216c330137eb7d5da713c47e785fa135
imogencs <40676815+imogencs@users.noreply.github.com>
2019-11-06 18:13:06 -0500

    Update working magic square .cpp
    works for part 4 bullet points 1, 2, and 3

--- working magic square .cpp
+++ working magic square .cpp
@@ -10,33 +10,46 @@
 #include "SuribachsMagicSquare.h"
 using namespace std;
 
-int *sizes = new int[16];
+int MAX_TARGET_SUM = 33;
+int **sizes = new int*[MAX_TARGET_SUM+1];
 
-void sumCombos(int currentSum, int numsInCombo, int *combo, int *cindex, int startIndex) {
+void sumCombos(int currentSum, int numsInCombo, int *combo, int startIndex, int targetSum) {
 	for (int i = startIndex; i < SQUARE_SIZE; i++) {
-		if (MAGIC_SQUARE[i] < (MAGIC_CONSTANT - currentSum)) {
+		if (MAGIC_SQUARE[i] < (targetSum - currentSum)) {
 			combo[numsInCombo - 1] = MAGIC_SQUARE[i];
-			sumCombos(currentSum+MAGIC_SQUARE[i], numsInCombo+1, combo, cindex,i+1);
+			sumCombos(currentSum+MAGIC_SQUARE[i], numsInCombo+1, combo, i+1, targetSum);
 		}
-		else if (MAGIC_SQUARE[i] == (MAGIC_CONSTANT - currentSum)) {
+		else if (MAGIC_SQUARE[i] == (targetSum - currentSum)) {
 			combo[numsInCombo - 1] = MAGIC_SQUARE[i];
-			sizes[numsInCombo]++;
+			sizes[targetSum][numsInCombo]++;
 		}
 	}
 }
 
+void combosForEachSum() {
+	int *combo = new int[SQUARE_SIZE];
+	for (int i = 0; i <= MAX_TARGET_SUM; i++) {
+		sumCombos(0, 1, combo, 0, i);
+	}
+}
+
 int main () {
-	for (int i = 0; i < 16; i++) {
-		sizes[i] = 0;
+	for (int i = 0; i <= MAX_TARGET_SUM; i++) {
+		sizes[i] = new int[SQUARE_SIZE];
+		for (int j = 0; j < SQUARE_SIZE; j++) {
+			sizes[i][j] = 0;
+		}
 	}
 
-	int *combo = new int[16];
-	int *cindex = new int[16];
-	sumCombos(0, 1, combo, cindex, 0);
 
-	for (int i = 1; i < SQUARE_SIZE; i++) {
-		cout << "Sums of " << MAGIC_CONSTANT << " for " << i << " elements:\t" << sizes[i] << endl;
+	int *combo = new int[SQUARE_SIZE];
+	combosForEachSum();
+
+	for (int targetSum = 0; targetSum <= MAX_TARGET_SUM; targetSum++) {
+		for (int i = 1; i < SQUARE_SIZE; i++) {
+			cout << "Sums of " << targetSum << " for " << i << " elements:\t";
+			cout << sizes[targetSum][i] << endl;
+		}
 	}
-
 	return 1;
 }
